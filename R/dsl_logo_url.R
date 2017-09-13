054e85f379bca0b7f1d5fd79cb3b7945dc2a2273
muschellij2 <muschellij2@gmail.com>
2017-09-13 16:30:11 -0400

    Added jhuds url when PR goes through.  https://github.com/jhudsl/jhudsl.github.io/pull/6

--- R/dsl_logo_url.R
+++ R/dsl_logo_url.R
@@ -7,6 +7,7 @@
 #' dsl_logo_url()
 #' dsl_logo_badge()
 dsl_logo_url = function() {
+  # url = "http://jhudatascience.org/images/logo.png"
   url = "http://johnmuschelli.com/jhudsl/inst/jhudsl_logo.png"
   return(url)
 }
