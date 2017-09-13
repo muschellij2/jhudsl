487f6a93fb7317b5de0231fd8257e1dab6a69730
muschellij2 <muschellij2@gmail.com>
2017-09-13 16:15:00 -0400

    Travis setup for releases.

--- R/use_dsl_logo.R
+++ R/use_dsl_logo.R
@@ -1,20 +1,24 @@
 #' Use JHU DSL Logo
 #'
 #' @param base_path Path to root directory
+#' @param ... arguments to pass to \code{\link{dsl_badge}}
 #'
 #' @return Invisible TRUE
 #' @export
+#' @importFrom usethis use_build_ignore
 use_dsl_logo = function(
-  base_path = ".") {
+  base_path = ".", ...) {
   logo = system.file("jhudsl_logo.png", package = "jhudsl")
-
+  fname =  basename(logo)
+  outfile = file.path(base_path, fname)
   usethis::use_build_ignore(
-    files = logo,
+    files = fname,
     base_path = base_path)
 
-  outfile = file.path(base_path, basename(logo))
   msg = paste0("Please add the following to your REAMDE.[R]md\n",
-               "![](", basename(logo), ")")
+               dsl_badge(logo = fname, ...)
+  )
+
   message(msg)
   file.copy(logo, outfile, overwrite = TRUE)
   return(invisible(TRUE))
